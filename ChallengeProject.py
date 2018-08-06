9913481ea3153cd08db4514a8b4eff9dc8fb9b20
PlantsVsDan <Soccer.Ivanovich@gmail.com>
2018-08-06 15:34:02 -0400

    Initial Commit
    Setup gitignore

---
+++ ChallengeProject.py
@@ -0,0 +1,17 @@
+"""
+#
+# File:              ChallengeProject.py
+# Author:            danIv (Daniel Ivanovich)
+# Created:           08/06/2018
+# Description:       Finds electrical energy used during nights in AHS for the calendar years 2016/17 and 2017/18.
+#
+"""
+
+import pandas as pd
+import os
+
+PART_1_PATH_2016 = os.path.join('CSVs', 'AHS201617Part1.csv')
+PART_2_PATH_2016 = os.path.join('CSVs', 'AHS201617Part2.csv')
+
+PART_1_PATH_2017 = os.path.join('CSVs', 'AHS201718Part1.csv')
+PART_2_PATH_2017 = os.path.join('CSVs', 'AHS201718Part2.csv')
